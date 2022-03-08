e814ca2ea7af0df6b4102395aa5da0a89e9a0a1a
yuanfang <yuanfang@minivision.cn>
2022-03-08 18:41:16 +0800

    Add 路由自动加载

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,14 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 const errorHandler = require('./errorHandler')
 
-const userRouter = require('../router/user.route')
+const router = require('../router/index')
+
 
 const app = new Koa()
 
 // middleware must be a function!
 app.use(koaBody())
-app.use(userRouter.routes())
+app.use(router.routes()).use(router.allowedMethods())
 
 // 统一的错误处理
 app.on('error', errorHandler)
